7a6dc7c87ed024b5fc6a953bcd6cd2398f93bc72
Jason <jason.b.harris1@gmail.com>
2022-06-25 08:50:32 -0500

    add service to get movies

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/550?api_key=824864242e7386dda207ea089b0a7e76')
+  }
 }
